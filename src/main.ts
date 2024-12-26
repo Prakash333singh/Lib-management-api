512383991a96c39a37301eabcb3f56bcb42affdf
Prakash Adhikari <pa7846665@gmail.com>
2024-12-26 23:41:42 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle("Library Management API")
+    .setDescription("API for managing library books")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
